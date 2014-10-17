e3afad02ed713bcf714407be522e6bd8bf216a16
Raquel <calderon.rocky@gmail.com>
2014-10-16 17:58:39 -0700

    Remove spaces from words

--- person_organization_enhancer.py
+++ person_organization_enhancer.py
@@ -13,21 +13,30 @@ org_links = data['definitions']['jmd:RelatedAgentObject']['jmd:organizationLinks
 
 import xml.etree.ElementTree as ET
 from urllib.request import urlopen
+import re
+
+# Library of Congress Search page
+LOC = 'http://authorities.loc.gov/cgi-bin/Pwebrecon.cgi?DB=local&PAGE=First'
 
 tree = ET.parse(urlopen('http://hydro10.sdsc.edu/metadata/National_Climatic_Data_Center/23759C9A-F801-495B-B140-9A41637E3D7C.xml'))
 root = tree.getroot()
 
-contacts = root.findall('{http://www.isotc211.org/2005/gmd}contact')
-orgs = []
-for c in contacts:
-    org = c.find('{http://www.isotc211.org/2005/gmd}CI_ResponsibleParty')
-    orgs.append(org)
+# find responsible party
+resp_party = root.find('{http://www.isotc211.org/2005/gmd}contact').find('{http://www.isotc211.org/2005/gmd}'
+                                                                        'CI_ResponsibleParty')
+# find organisationName field and the value in it
+org_field_val = resp_party.find('{http://www.isotc211.org/2005/gmd}organisationName').find('{http://www.isotc211.org/'
+                                                                                           '2005/gco}CharacterString').text
+orgs = re.split('[^a-zA-Z\s\d:.]', org_field_val)
 
-total_org_names = []
-for o in orgs:
-    org_name = o.find('{http://www.isotc211.org/2005/gmd}organisationName').find('{http://www.isotc211.org/2005/gco}'
-                                                                                 'CharacterString').text
-    total_org_names.append(org_name)
+for org in orgs:
+    if re.match('(^\s|\s$)', org):
+        index = orgs.index(org)
+        org_with_spaces = orgs.pop(index)
+        # remove spaces from org
+        new_org = org_with_spaces.rstrip().lstrip()
+        orgs.insert(index, new_org)
 
-for org_name in total_org_names:
-    print(org_name)
+# Now we have a list of orgs with no whitespaces
+# for each in orgs:
+    # make request to library of congress
