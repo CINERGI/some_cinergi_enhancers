019f6a0df83f529b5c29377defe78eab21e0c80f
Raquel <calderon.rocky@gmail.com>
2014-10-21 12:34:14 -0700

    Sends request to LOC and receives page of results back

--- person_organization_enhancer.py
+++ person_organization_enhancer.py
@@ -1,5 +1,11 @@
 __author__ = 'Raquel'
 
+import xml.etree.ElementTree as ET
+from bs4 import BeautifulSoup
+from urllib.request import urlopen, Request
+import urllib.parse
+import re
+
 """
 # Code for eventual working with json
 import json
@@ -11,12 +17,9 @@ org_URI = data['definitions']['jmd:RelatedAgentObject']['jmd:organizationURI']
 org_links = data['definitions']['jmd:RelatedAgentObject']['jmd:organizationLinks']
 """
 
-import xml.etree.ElementTree as ET
-from urllib.request import urlopen
-import re
-
 # Library of Congress Search page
-LOC = 'http://authorities.loc.gov/cgi-bin/Pwebrecon.cgi?DB=local&PAGE=First'
+loc_url = 'http://authorities.loc.gov/cgi-bin/Pwebrecon.cgi?'
+loc_search_page = 'http://authorities.loc.gov/cgi-bin/Pwebrecon.cgi?DB=local&PAGE=First'
 
 tree = ET.parse(urlopen('http://hydro10.sdsc.edu/metadata/National_Climatic_Data_Center/23759C9A-F801-495B-B140-9A41637E3D7C.xml'))
 root = tree.getroot()
@@ -37,6 +40,30 @@ for org in orgs:
         new_org = org_with_spaces.rstrip().lstrip()
         orgs.insert(index, new_org)
 
+# Grab the PID and SEQ from LOC search page because they don't want me to be successful
+search_page = BeautifulSoup(urlopen(loc_search_page).read())
+PID = search_page.find('input', {'name': 'PID'})['value']
+SEQ = search_page.find('input', {'name': 'SEQ'})['value']
+print(PID)
+print(SEQ)
+
+# Holds the response pages returned by our requests
+pages = []
 # Now we have a list of orgs with no whitespaces
-# for each in orgs:
-    # make request to library of congress
+for each in orgs:
+    values = {'Search_Arg': each,
+              'Search_Code': 'NHED_',
+              'PID': PID,
+              'SEQ': SEQ,
+              'CNT': 100,
+              'HIST': 1}
+    myData = urllib.parse.urlencode(values)
+    data = myData.encode('utf-8')
+    req = Request(loc_url, data)
+    response = urlopen(req)
+    the_page = BeautifulSoup(response.read()).prettify()
+    pages.append(the_page)
+
+f = open('output.txt', 'r+')
+f.write(pages[len(pages)-1])
+f.close()
