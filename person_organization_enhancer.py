02a719174430ec77d05196685a74c911b4d7163e
Raquel <calderon.rocky@gmail.com>
2014-10-26 16:31:49 -0700

    Searches for individuals in orcid

--- person_organization_enhancer.py
+++ person_organization_enhancer.py
@@ -2,7 +2,7 @@ __author__ = 'Raquel'
 
 import xml.etree.ElementTree as ET
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
+from urllib.request import urlopen, Request
 import urllib.parse
 import re
 
@@ -17,16 +17,19 @@ org_URI = data['definitions']['jmd:RelatedAgentObject']['jmd:organizationURI']
 org_links = data['definitions']['jmd:RelatedAgentObject']['jmd:organizationLinks']
 """
 
-# Present library of congress search page
+# Library of congress search page
 loc_url = 'http://id.loc.gov/search/'
+
+# Orcid search page
+orcid_url = 'https://orcid.org/orcid-search/quick-search'
 
 # NCDC resource
 # http://hydro10.sdsc.edu/metadata/National_Climatic_Data_Center/23759C9A-F801-495B-B140-9A41637E3D7C.xml
 # CZO resource
 # http://hydro10.sdsc.edu/metadata/Critical_Zone_Observatory_Catalog/159C0A40-C9AC-4161-914B-193FBAC9C1D1.xml
 
-tree = ET.parse(urlopen('http://hydro10.sdsc.edu/metadata/National_Climatic_Data_Center/'
-                        '23759C9A-F801-495B-B140-9A41637E3D7C.xml'))
+tree = ET.parse(urlopen('http://hydro10.sdsc.edu/metadata/Critical_Zone_Observatory_Catalog/'
+                        '159C0A40-C9AC-4161-914B-193FBAC9C1D1.xml'))
 root = tree.getroot()
 
 # find two elements with Responsible Party: contact and identification info
@@ -48,18 +51,26 @@ org_elements.append(contact_orgs)
 citation = idInfo.find('{http://www.isotc211.org/2005/gmd}citation').find('{http://www.isotc211.org/2005/gmd}'
                                                                           'CI_Citation')
 
-citation_orgs = citation.find('{http://www.isotc211.org/2005/gmd}citedResponsibleParty').find(
-    '{http://www.isotc211.org/2005/gmd}CI_ResponsibleParty').find('{http://www.isotc211.org/2005/gmd}'
-                                                                  'organisationName').find('{http://www.isotc211.org/'
-                                                                                           '2005/gco}CharacterString')
+title = citation.find('{http://www.isotc211.org/2005/gmd}title').find('{http://www.isotc211.org/2005/gco}'
+                                                                      'CharacterString').text
 
-org_elements.append(citation_orgs)
+citation_rp = citation.find('{http://www.isotc211.org/2005/gmd}citedResponsibleParty')
 
-pointOfContact_orgs = idInfo.find('{http://www.isotc211.org/2005/gmd}pointOfContact'). \
-    find('{http://www.isotc211.org/2005/gmd}CI_ResponsibleParty').find('{http://www.isotc211.org/'
-                                                                       '2005/gmd}organisationName').find(
-    '{http://www.isotc211.org/2005/gco}CharacterString')
-org_elements.append(citation_orgs)
+if citation_rp is not None:
+    more_orgs = citation_rp.find('{http://www.isotc211.org/2005/gmd}CI_ResponsibleParty').\
+        find('{http://www.isotc211.org/2005/gmd}organisationName').\
+        find('{http://www.isotc211.org/2005/gco}CharacterString')
+    org_elements.append(more_orgs)
+
+# Find all points of contact
+pointsOfContact = idInfo.findall('{http://www.isotc211.org/2005/gmd}pointOfContact')
+
+for each in pointsOfContact:
+    orgName = each.find('{http://www.isotc211.org/2005/gmd}'
+                        'CI_ResponsibleParty').find('{http://www.isotc211.org/2005/gmd}organisationName')
+    if orgName is not None:
+        org_elements.append(orgName.find('{http://www.isotc211.org/2005/gco}CharacterString'))
+
 
 # List to hold strings of organization names
 orgs = []
@@ -79,7 +90,17 @@ for org in orgs:
         new_org = org_with_spaces.rstrip().lstrip()
         orgs.insert(index, new_org)
 
-print(orgs)
+names = []
+for p in pointsOfContact:
+    ci_respParty = p.find('{http://www.isotc211.org/2005/gmd}CI_ResponsibleParty')
+    indiv = ci_respParty.find('{http://www.isotc211.org/2005/gmd}individualName')
+    if indiv:
+        name = indiv.find('{http://www.isotc211.org/2005/gco}CharacterString').text
+    if name not in names:
+        names.append(name)
+
+print(names)
+
 # Send each in a makeshift post request to Library of Congress db of authority names
 # Problem: Query uses the same name ('q') for the keyword as well as the options
 # for what to search. This is solved by encoding and adding to the url twice
@@ -94,5 +115,16 @@ for each in orgs:
     full_url = url_w_name + '&' + options
     the_page = BeautifulSoup(urlopen(full_url).read()).prettify()
     f = open('output_{}.txt'.format(each), 'w+')
+    f.write(the_page)
+    f.close()
+
+
+for each in names:
+    search_val = {'keys': each}
+    data = urllib.parse.urlencode(search_val)
+    myData = data.encode('utf-8')
+    req = Request(orcid_url, myData)
+    the_page = BeautifulSoup(urlopen(req).read()).prettify()
+    f = open('output_{}.text'.format(each), 'w+')
     f.write(the_page)
     f.close()
